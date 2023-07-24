fb85fde686574bd74d90f37c8cb74e67d8ded081
zuprizal <zuprizalkurniawan03@gmail.com>
2023-07-24 21:56:58 +0700

    first commit

---
+++ app/src/main/java/com/example/utshad/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.utshad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.example.utshad.databinding.ActivityMainBinding
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getRandomQuote()
+
+    }
+
+private fun getRandomQuote() {
+    binding.progressBar.visibility = View.VISIBLE
+    val client = AsyncHttpClient()
+    val url = "http://localhost/had/juicemp.php"
+    client.get(url, object : AsyncHttpResponseHandler() {
+        override fun onSuccess(
+            statusCode: Int,
+            headers: Array<out cz.msebera.android.httpclient.Header>?,
+            responseBody: ByteArray?
+        ) {
+            binding.progressBar.visibility = View.INVISIBLE
+            val result = String(responseBody!!)
+            Log.d(TAG, result)
+            try {
+                val responseObject = JSONObject(result)
+                val nim = responseObject.getString("nim")
+                val nama = responseObject.getString("nama")
+                val alamat = responseObject.getString("alamat")
+                val nohp = responseObject.getString("nohp")
+                binding.nim.text = nim
+                binding.nama.text = nama
+                binding.alamat.text = alamat
+                binding.nohp.text = nohp
+
+            } catch (e: Exception) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                e.printStackTrace()
+            }
+        }
+
+        override fun onFailure(
+            statusCode: Int,
+            headers: Array<out cz.msebera.android.httpclient.Header>?,
+            responseBody: ByteArray?,
+            error: Throwable?
+        ) {
+            binding.progressBar.visibility = View.INVISIBLE
+            val errorMessage = when (statusCode) {
+                401 -> "$statusCode : Bad Request"
+                403 -> "$statusCode : Forbidden"
+                404 -> "$statusCode : Not Found"
+                else -> "$statusCode : ${error?.message}"
+            }
+            Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
+        }
+
+    })
+
+}}
